90858b5e9b583bdf7cd5e091131381e996441d6d
Tyler Huyser <tyler.huyser@gmail.com>
2020-11-02 00:27:33 -0500

    deployed to Heroku

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://awoken-app.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
