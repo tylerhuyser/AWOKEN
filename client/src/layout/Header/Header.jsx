42ba5500fbd267afede9d3b72d3fee022eae42e7
Tyler Huyser <tyler.huyser@gmail.com>
2023-05-09 18:12:19 -0400

    edited Header component.

--- client/src/layout/Header/Header.jsx
+++ client/src/layout/Header/Header.jsx
@@ -17,31 +17,24 @@ export default function Header(props) {
   }
 
   return (
-
-    <>
-
-    <div className="header-container-mobile slide-in-top-header mobile"> 
-      <i className="fas fa-bars"></i>
-      <img alt="wims-logo" src="https://i.imgur.com/ioUfIYI.png" width="60vw" />
-      <i className="far fa-user-circle" onClick={() => handleLogout(history, setCurrentUser)}></i>
-    </div>
-
-    <div className="header-container-desktop slide-in-top-header desktop">
       
-      <div className="menu-login-container">
-          
-          {toggleDesktopMenu ? <i className="fas fa-times" onClick={changeDesktopMenuVisibility}></i> : <i className="fas fa-bars" onClick={changeDesktopMenuVisibility}></i>}
-          
-        <img alt="wims-logo" src="https://i.imgur.com/ioUfIYI.png" width="50vw" />
+    <div className="header-container"> 
+        
+      <div className='header-menu-icon-container'>
+        <i className="header-icon mobile fas fa-bars" id="header-menu-icon"></i>
+        {toggleDesktopMenu ? <i className="header-icon desktop fas fa-times" id="header-menu-icon" onClick={changeDesktopMenuVisibility}></i> : <i className="header-icon desktop fas fa-bars" id="header-menu-icon" onClick={changeDesktopMenuVisibility}></i>}
+      </div>
+
+      <div className='header-logo-container'>
+        <img className='header-logo' alt="AWOKEN-header-logo" src="/images/logos/AWOKEN-logo-orange.png" />
       </div>
         
-      <div className="current-user-header-container">
-        <i className="fas fa-bell"></i>
-        <div className="current-user-name" onClick={handleLogout}>{currentUser.first_name} {currentUser.last_name}</div>
+      <div className="header-current-user-container">
+        <i className="header-icon mobile far fa-user-circle" onClick={() => handleLogout(history, setCurrentUser)}></i>
+        <i className="header-icon desktop fas fa-bell"></i>
+        <div className="current-user-name desktop" onClick={handleLogout}>{currentUser.first_name} {currentUser.last_name}</div>
       </div>
         
     </div>
-      
-    </>
   )
 }
