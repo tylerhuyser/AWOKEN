1e5a17e9d91840ee68c2de6fe5970f535cc9951e
Tyler Huyser <tyler.huyser@gmail.com>
2023-05-10 20:47:13 -0400

    edits to Home css.

---
+++ client/src/functions/beginJournal.js
@@ -0,0 +1,7 @@
+import handleNavigation from './handleNavigation';
+
+export default function beginJournal(e, history, setPendingSurvey) {
+  e.preventDefault();
+  setPendingSurvey(true)
+  handleNavigation(history, '/new-journal')
+}
